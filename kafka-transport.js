a46eec62ef4c7224dc46d158e6548b5987328983
Chico Charlesworth <chico.charlesworth@googlemail.com>
2014-08-05 11:45:52 +0100

    Fixing kafka transport to work with latest seneca transport version

--- kafka-transport.js
+++ kafka-transport.js
@@ -12,6 +12,11 @@ module.exports = function(options) {
     seneca.use('transport');
   }
 
+  var tu = seneca.export('transport/utils');
+
+  seneca.add({role:'transport',hook:'listen',type:'kafka'}, hookListenQueue);
+  seneca.add({role:'transport',hook:'client',type:'kafka'}, hookClientQueue);
+
   function hookListenQueue(args, done) {
     listenBus = require('microbial')(options.microbial);
 
@@ -32,35 +37,47 @@ module.exports = function(options) {
     });
   }
 
-  function hookClientQueue(args, done) {
+  function hookClientQueue(args, clientdone) {
     var seneca = this;
+    var type           = args.type
+    var client_options = seneca.util.clean(_.extend({},options[type],args));
+
     var callmap = {};
     clientBus = require('microbial')(options.microbial);
 
-    clientBus.run([{group: options.kafka.group, topicName: options.kafka.responseTopic, responseChannel: true}], [], function(err) {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        var client = function(args, done) {
-          var outmsg = {
-            id:   nid(),
-            kind: 'act',
-            act:  args
+    tu.make_client( make_send, client_options, clientdone );
+
+    function make_send( spec, topic, send_done ) {
+
+      clientBus.run([{group: options.kafka.group, topicName: options.kafka.responseTopic, responseChannel: true}], [], function(err) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          var client = function(args, done) {
+            var outmsg = {
+              id:   nid(),
+              kind: 'act',
+              act:  args
+            };
+            callmap[outmsg.id] = {done:done};
+            clientBus.request({topicName: options.kafka.requestTopic}, outmsg, function(res) {
+              var call = callmap[res.response.id];
+              if( call ) {
+                delete callmap[res.response.id];
+                call.done(res.response.err ? new Error(res.response.err) : null, res.response.res);
+              }
+            });
           };
-          callmap[outmsg.id] = {done:done};
-          clientBus.request({topicName: options.kafka.requestTopic}, outmsg, function(res) {
-            var call = callmap[res.response.id];
-            if( call ) {
-              delete callmap[res.response.id];
-              call.done(res.response.err ? new Error(res.response.err) : null, res.response.res);
-            }
-          });
-        };
-        seneca.log.info('client', 'pubsub', args.host, args.port, seneca.toString());
-        done(null,client);
-      }
-    });
+          seneca.log.info('client', 'pubsub', args.host, args.port, seneca.toString());
+
+          send_done( null, function( args, done ) {
+            client.call(this, args, done);
+          })
+        }
+      });
+
+    }
   }
 
   var shutdown = function(args, done) {
@@ -76,10 +93,8 @@ module.exports = function(options) {
     }
   };
 
-  seneca.add({role:'transport',hook:'listen',type:'queue'}, hookListenQueue);
-  seneca.add({role:'transport',hook:'client',type:'queue'}, hookClientQueue);
   seneca.add({role:'seneca',cmd:'close'}, shutdown);
- 
+
   return {
     name: plugin,
   };
